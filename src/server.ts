f0b8f65c7f06605d70c7fb8af7d69c77668a9eae
Luiz Felipe Escandiuzzi <luiz.escandiuzzi@duo.studio>
2021-07-21 08:25:49 -0300

    Changed app port

--- src/server.ts
+++ src/server.ts
@@ -36,4 +36,4 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
 })
 
 
-app.listen(3000, () => console.log("Server is running"));
+app.listen(process.env['PORT'] || 5000, () => console.log("Server is running"));
